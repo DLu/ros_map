a98487e19ba842aeee39568228c4049776221460
David V. Lu!! <davidvlu@gmail.com>
2014-07-28 15:36:07 -0500

    Add addresses, links, descriptions, style

--- generate_kml.py
+++ generate_kml.py
@@ -5,6 +5,9 @@ from yaml import load
 import sys
 dom = Document()
 
+STYLES = {'school': '#icon-959-62AF44', 'company': '#icon-959-4186F0', 'other': '#icon-959-FFDD5E'}
+DEFAULT_STYLE = '#icon-959-FFFFFF'
+
 def text_element(name, text, cdata=False):
     if text is None:
         text = ''
@@ -15,19 +18,35 @@ def text_element(name, text, cdata=False):
         d.appendChild( dom.createTextNode(text.encode('utf-8')) )
     return d
 
+def data_element(name, value):
+    data = dom.createElement('Data')
+    data.setAttribute('name', name)
+    data.appendChild( text_element('value', value) )
+    return data
+
 def create_folder(name, data):
     f = dom.createElement('Folder')
     f.appendChild( text_element('name', name))
     for place in data:
         p = dom.createElement('Placemark')
+        style = STYLES.get(place['type'], DEFAULT_STYLE)
+        p.appendChild( text_element('styleUrl', style))
         p.appendChild( text_element('name', place['name']))
         
         edata = dom.createElement('ExtendedData')
-        data = dom.createElement('Data')
-        data.setAttribute('name', 'type')
-        data.appendChild( text_element('value', place['type']) )
-        edata.appendChild(data)
+        edata.appendChild( data_element('type', place['type']) )
+        if 'link' in place:
+            edata.appendChild( data_element('gx_media_links', place['link']) )
+
         p.appendChild(edata)
+
+        if 'address' in place:
+            p.appendChild( text_element('address', place['address']) )
+
+        if 'description' in place:
+            d = dom.createElement('description')
+            d.appendChild( dom.createCDATASection(place['description']) )
+            p.appendChild(d)
         
         point = dom.createElement('Point')
         p.appendChild(point)
@@ -35,6 +54,18 @@ def create_folder(name, data):
         
         f.appendChild(p)
     
+    return f
+
+def create_style(name, color, url='http://www.gstatic.com/mapspro/images/stock/959-wht-circle-blank.png'):
+    f = dom.createElement('Style')
+    f.setAttribute('id', name)
+    p = dom.createElement('IconStyle')
+    f.appendChild(p)
+    p.appendChild( text_element('color', 'ff' + color) )
+    p.appendChild( text_element('scale', '1.1') )
+    i = dom.createElement('Icon')
+    p.appendChild( i )
+    i.appendChild( text_element('href', url) )
     return f
 
 root = dom.createElement("kml")
@@ -46,6 +77,10 @@ document.appendChild( text_element('name', 'ROS Users of the World') )
 document.appendChild( text_element('description', 'ROS Users of the World', True) )
 document.appendChild( create_folder('ROS Users', load(open(sys.argv[1]))) )
 
-
+document.appendChild( create_style('icon-959-4186F0', 'F08641') )
+document.appendChild( create_style('icon-959-62AF44', '44AF62') )
+document.appendChild( create_style('icon-959-FFDD5E', '5EDDFF') )
+document.appendChild( create_style('icon-959-FFFFFF', 'FFFFFF') )
 
 print dom.toprettyxml()
+
